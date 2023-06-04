5a80cbcfd5c369aeecb0bf66594f58c6c4ac792d
Tudor Huza <huzatudor@gmail.com>
2023-06-04 16:25:38 +0300

    Fixed 'Robot' project's folder's nameAdd project files.

---
+++ Robot/Robots/Robot.cs
@@ -0,0 +1,16 @@
+﻿namespace Robot.Robots
+{
+
+    public class Robot
+    {
+
+        public bool Active { get; private set; }
+
+        public Robot() { }
+
+        public void initialize()
+        {
+            Active = true;
+        }
+    }
+}
